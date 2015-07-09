f3a65f7881b6fda2b03bb3d942f50b6d9c7b3748
owuor91 <owuor91@gmail.com>
2015-07-09 10:50:13 +0300

    add FileValidatorActor.cs to replace validationActor

---
+++ FileValidatorActor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Akka.Actor;
+
+namespace WinTail
+{
+    class FileValidatorActor : UntypedActor
+    {
+        private readonly IActorRef _consoleWriterActor;
+        private readonly IActorRef _tailCoordinatorActor;
+
+        private FileValidatorActor(IActorRef consoleWriterActor, IActorRef tailCoordinatorActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+            _tailCoordinatorActor = tailCoordinatorActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+            if (string.IsNullOrEmpty(msg))
+            {
+                _consoleWriterActor.Tell(new Messages.InputError.NullInputError("Blank input, type something mate..."));
+                Sender.Tell(new Messages.ContinueProcessing());
+            }
+            else
+            {
+                var valid = IsFileUri(msg);
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess(string.Format("Start processing for {0}",msg)));
+                    _tailCoordinatorActor.Tell(new TailCoordinatorActor.StartTail(msg, _consoleWriterActor));
+                }
+                else
+                {
+                    _tailCoordinatorActor.Tell(new Messages.InputError.ValidationError(string.Format("{0} is not a valid URI on disk", msg)));
+                    Sender.Tell(new Messages.ContinueProcessing());
+                }
+            }
+        }
+
+        private static bool IsFileUri(string path)
+        {
+            return File.Exists(path);
+        }
+    }
+}

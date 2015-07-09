3fd7c97a401da3d0f183273eda6353c10be23d83
owuor91 <owuor91@gmail.com>
2015-07-09 12:08:58 +0300

    Add TailCoordinatorActor class

---
+++ TailCoordinatorActor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace WinTail
+{
+    class TailCoordinatorActor : UntypedActor
+    {
+        public class StartTail
+        {
+            public StartTail(string filePath, IActorRef reporterActor)
+            {
+                FilePath = filePath;
+                ReporterActor = reporterActor;
+            }
+
+            public string FilePath { get; private set; }
+            public IActorRef ReporterActor { get; private set; }
+        }
+
+        public class StopTail
+        {
+            public StopTail(string filepath)
+            {
+                Filepath = filepath;
+            }
+
+            public string Filepath { get; private set; }
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is StartTail)
+            {
+                var msg = message as StartTail;
+            }
+        }
+    }
+}
